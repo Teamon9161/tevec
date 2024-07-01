d6822e64edb00eca3f0af377726c0858f1f29abd
Teamon9161 <1127972945@qq.com>
2024-07-01 15:27:32 +0800

    fix build

--- tevec/build.rs
+++ tevec/build.rs
@@ -2,10 +2,8 @@ fn main() {
     #[cfg(feature = "ffi")]
     {
         cxx_build::bridge("src/ffi.rs")
-            .std("c11")
-            // .file("src/include/t_ffi.cpp")
             .include("src/include/special")
-            // .flag_if_supported("-std=c++11")
+            .flag_if_supported("-std=c++11")
             .compile("t_ffi")
     }
 }
