d207a42824d046969166ed6671a828f82e363024
Teamon9161 <1127972945@qq.com>
2024-11-18 10:07:02 +0800

    [fix] fix half life, add test for half life

--- tevec/src/agg.rs
+++ tevec/src/agg.rs
@@ -71,8 +71,12 @@ pub trait AggValidFinal<T: IsNone>: Vec1View<T> {
         let mut n: usize = 0;
         let mut last_n = 0;
         let mut i = 0;
-        let min_periods = min_periods.unwrap_or(self.len() / 2);
-        while n < self.len() {
+        let len = self.len();
+        if len == 0 {
+            return 0;
+        }
+        let min_periods = min_periods.unwrap_or(len / 2);
+        while n < len {
             n = 2usize.pow(i);
             let s_shift = self.titer().vshift(n as i32, None);
             let corr: f64 = self.titer().vcorr_pearson(s_shift, min_periods);
@@ -114,7 +118,11 @@ mod tests {
     fn test_half_life() {
         let s = vec![10., 12., 13., 14., 15., 12., 11., 14., 15., 16.];
         assert_eq!(s.ts_vmean::<Vec<f64>, _>(4, Some(1)).half_life(Some(1)), 3);
+        // test empty vec
         let s: Vec<f64> = vec![];
+        assert_eq!(s.ts_vmean::<Vec<f64>, _>(4, Some(1)).half_life(Some(1)), 0);
+        // test all nan
+        let s = vec![f64::NAN; 10];
         assert_eq!(s.ts_vmean::<Vec<f64>, _>(4, Some(1)).half_life(Some(1)), 1);
     }
 }
